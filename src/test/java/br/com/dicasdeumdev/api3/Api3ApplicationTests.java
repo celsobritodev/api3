3e392a964a99c80fc45aa246cee27fda6555e087
celsobritodev <celsinhobrito@gmail.com>
2024-06-30 20:46:08 -0300

    primeiro commit

---
+++ src/test/java/br/com/dicasdeumdev/api3/Api3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dicasdeumdev.api3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
