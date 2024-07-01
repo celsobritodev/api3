f52737c7ec54510ed1e8f749c6a346a0f6d6fa86
celsobritodev <celsinhobrito@gmail.com>
2024-06-30 21:43:29 -0300

    segundo commit

--- src/main/java/br/com/dicasdeumdev/api3/Api3Application.java
+++ src/main/java/br/com/dicasdeumdev/api3/Api3Application.java
@@ -3,11 +3,23 @@ package br.com.dicasdeumdev.api3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import domain.User;
+
+
+
+
+
 @SpringBootApplication
 public class Api3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Api3Application.class, args);
+		
+		//User user = new User(1,"valdir","valdir@email.com","123");
+		//User user = new User();
+		//User user = new User(1,"","","");
+		//User user = new User();
+		
 	}
 
 }
